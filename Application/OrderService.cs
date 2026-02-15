705c6dd04ca90701dd492782a78d01288f972afa
hugo ricardo cardenas diaz gonzalez <hugo_chivas11_16@hotmail.com>
2026-02-14 20:25:54 -0600

    Add comments

--- Application/OrderService.cs
+++ Application/OrderService.cs
@@ -12,28 +12,55 @@ namespace Application
             _orderRepository = orderRepository;
         }
 
+        /// <summary>
+        /// Creacion de orden
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
         public int CreateOrder(Order order)
         {
             _orderRepository.CreateOrder(order);
             return 1;
         }
 
+        /// <summary>
+        /// Eliminar orden
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public int DeleteOrder(int id)
         {
             _orderRepository.DeleteOrder(1);
             return 0;
         }
 
+        /// <summary>
+        /// Obtener order por Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public Order GetOrder(int id)
         {
             return _orderRepository.GetOrder(id);
         }
 
+        /// <summary>
+        /// Obtener ordenes
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public int GetOrderders(Order order)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Actualizar orden
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
         public int UpdateOrder(Order order)
         {
             throw new NotImplementedException();
